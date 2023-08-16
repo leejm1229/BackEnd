c72d467220947ef3beb0d0ae6e293b1948f8732b
leejm1229 <ljm5524@gmail.com>
2023-08-16 14:22:22 +0900

    xml 공부

--- springdi/src/main/java/basic/HelloMain.java
+++ springdi/src/main/java/basic/HelloMain.java
@@ -14,6 +14,13 @@ public class HelloMain {
 		// 생성자는 한번만 출력된다.
 		Hello hh = (Hello)context.getBean("h1");
 		hh.printHello();
+		
+		// 이건 다른거기 때문에 생성자가 출력된다.
+		Hello hhh = (Hello)context.getBean("h2");
+		hhh.printHello();
+		
+		System.out.println(h);
+		System.out.println(hhh);
 
 	}
 
