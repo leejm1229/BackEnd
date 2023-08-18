d9fd60b47a9678c642cd22c1a0268df5c967be61
leejm1229 <ljm5524@gmail.com>
2023-08-18 15:39:14 +0900

    update delete 추가

--- mybatis/src/mybatis/BoardDAO.java
+++ mybatis/src/mybatis/BoardDAO.java
@@ -18,13 +18,21 @@ public class BoardDAO {
     public void work() {    
         
         // select all 호출
-        // selectNewPost();
+         selectNewPost();
         
         // select byName 호출
-        selectbyName();
+//        selectbyName();
+    	
+//         select byName2 호출
+//         selectbyName2();
         
+    	// selectbyNo 호출 
+//    	selectbyNo();
+    	
         // insert 호출
-        //insertNewPost();
+//        insertNewPost();
+//    	updateNo();
+//    	deleteNo();
     }
     
     // select
@@ -47,7 +55,24 @@ public class BoardDAO {
         for(BoardVO board : boardList) {
             System.out.println(board);
         }
+    }
+    
+    public void selectbyName2() {
+        List<BoardVO> boardList = 
+                sqlSession.selectList("mybatis.BoardDAO.selectbyName2", "김길동");
         
+        for(BoardVO board : boardList) {
+            System.out.println(board);
+        }
+    }
+    
+    public void selectbyNo() {
+        List<BoardVO> boardList = 
+                sqlSession.selectList("mybatis.BoardDAO.selectbyNo", 10);
+        
+        for(BoardVO board : boardList) {
+            System.out.println(board);
+        }
     }
     
     // insert
@@ -61,6 +86,18 @@ public class BoardDAO {
         sqlSession.insert("mybatis.BoardDAO.insertNewPost2", boardVO);
         
         sqlSession.commit();
+    }
+    
+    // update
+    public void updateNo() {	
+    	sqlSession.update("mybatis.BoardDAO.updatebyNo", 5);
+    	sqlSession.commit();
+    }
+    
+ // delete
+    public void deleteNo() {
+    	sqlSession.update("mybatis.BoardDAO.deletebyNo", 3);
+    	sqlSession.commit();
     }
     
 
