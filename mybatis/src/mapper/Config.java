7bdd37d9be38361fd76577ac01b46332d5801d86
leejm1229 <ljm5524@gmail.com>
2023-08-18 09:06:40 +0900

    mybatis 설정

---
+++ mybatis/src/mapper/Config.java
@@ -0,0 +1,19 @@
+package mapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Config {
+	private SqlSession sqlSeesSession;
+
+	public Config() throws IOException  {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+}
---
+++ mybatis/src/mapper/MybatisMain.java
@@ -0,0 +1,8 @@
+package mapper;
+
+public class MybatisMain {
+	public static void main(String[] args) {
+		
+		
+	}
+}
